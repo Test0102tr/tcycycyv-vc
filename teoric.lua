5b2168925a2718132c4790f73458f0bd9095be11
Test0102tr <106497002+Test0102tr@users.noreply.github.com>
2023-03-08 10:00:07 -0300

    lixo

--- teoric.lua
+++ teoric.lua
@@ -11,108 +11,115 @@ if game.PlaceId == 5712833750 then
 _G.Coins = true
 _G.LavaMonster = true
 _G.Griffin = true
-        
-        ---function
-function Coins()
-        while _G.Coins == true do
+        function Coins()
+    while CoinsFlag == true do
         game:GetService("ReplicatedStorage").Events.CoinEvent:FireServer()
-        wait(1)
-        end
+        delay(1)
+    end
 end
+
 function LavaMonster()
-        while _G.LavaMonster == true do
+    while LavaMonsterFlag == true do
         local args = {
-    [1] = workspace.NPC.LavaGorilla.Humanoid,
-    [2] = 1
-}
-
-game:GetService("ReplicatedStorage").jdskhfsIIIllliiIIIdchgdIiIIIlIlIli:FireServer(unpack(args))
-        wait(0.001)
-        end
+            [1] = workspace.NPC.LavaGorilla.Humanoid,
+            [2] = 1
+        }
+        game:GetService("ReplicatedStorage").jdskhfsIIIllliiIIIdchgdIiIIIlIlIli:FireServer(unpack(args))
+        delay(0.001)
+    end
 end
+
 function Griffin()
-        while _G.Griffin == true do
+    while GriffinFlag == true do
         local args = {
-    [1] = workspace.NPC.Griffin.Humanoid,
-    [2] = 1
-}
-
-game:GetService("ReplicatedStorage").jdskhfsIIIllliiIIIdchgdIiIIIlIlIli:FireServer(unpack(args))
-        wait(0.001)
-        end
+            [1] = workspace.NPC.Griffin.Humanoid,
+            [2] = 1
+        }
+        game:GetService("ReplicatedStorage").jdskhfsIIIllliiIIIdchgdIiIIIlIlIli:FireServer(unpack(args))
+        delay(0.001)
+    end
 end
 
-        --Jogador
-        local JogadorTab = Window:MakeTab({
-	Name = "Jogador",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-        local Section = JogadorTab:AddSection({
-	Name = "Auto-Farm"
-})
-        JogadorTab:AddToggle({
-	Name = "Auto Level/Coins",
-	Default = false,
-	Callback = function(Value)
-		_G.Coins = Value
-Coins()
-	end    
+local CoinsFlag = false
+local LavaMonsterFlag = false
+local GriffinFlag = false
+
+local JogadorTab = Window:MakeTab({
+    Name = "Jogador",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
-        --Others
-        local OthersTab = Window:MakeTab({
-	Name = "Others",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-        local Section = OthersTab:AddSection({
-	Name = "Others"
-})
-        OthersTab:AddButton({
-	Name = "RTX",
-	Callback = function()
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/Troll-Hub/Scripts/main/RTX.lua'))()
-
-
-  	end    
-})
-        OthersTab:AddButton({
-	Name = "RTX 2",
-	Callback = function()
-      		loadstring(game:HttpGet('https://raw.githubusercontent.com/Troll-Hub/Scripts/main/RTX_SCRIPT_v3.txt'))()
-  	end    
-})
-        OthersTab:AddButton({
-	Name = "Infinite Yield",
-	Callback = function()
-      		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-  	end    
-})
-        OthersTab:AddButton({
-	Name = "Destroy Hub",
-	Callback = function()
-      		OrionLib:Destroy()
-  	end    
+local Section = JogadorTab:AddSection({
+    Name = "Auto-Farm"
 })
 
-        --Beta
-        local BetaTab = Window:MakeTab({
-	Name = "Beta",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+JogadorTab:AddToggle({
+    Name = "Auto Level/Coins",
+    Default = false,
+    Callback = function(Value)
+        CoinsFlag = Value
+        Coins()
+    end    
 })
-        local Section = BetaTab:AddSection({
-	Name = "Beta functions"
+
+local OthersTab = Window:MakeTab({
+    Name = "Others",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
-        BetaTab:AddToggle({
-	Name = "Auto Lava Monster",
-	Default = false,
-	Callback = function(Value)
-		_G.LavaMonster = Value
-LavaMonster()
-	end    
+
+local Section = OthersTab:AddSection({
+    Name = "Others"
 })
+
+OthersTab:AddButton({
+    Name = "RTX",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Troll-Hub/Scripts/main/RTX.lua'))()
+    end    
+})
+
+OthersTab:AddButton({
+    Name = "RTX 2",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Troll-Hub/Scripts/main/RTX_SCRIPT_v3.txt'))()
+    end    
+})
+
+OthersTab:AddButton({
+    Name = "Infinite Yield",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    end    
+})
+
+OthersTab:AddButton({
+    Name = "Destroy Hub",
+    Callback = function()
+        OrionLib:Destroy()
+    end    
+})
+
+local BetaTab = Window:MakeTab({
+    Name = "Beta",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local Section = BetaTab:AddSection({
+    Name = "Beta functions"
+})
+
+BetaTab:AddToggle({
+    Name = "Auto Lava Monster",
+    Default = false,
+    Callback = function(Value)
+        LavaMonsterFlag = Value
+        LavaMonster()
+    end    
+})
+
         BetaTab:AddButton({
 	Name = "Tp to spawn Gorilla",
 	Callback = function()
@@ -194,14 +201,41 @@ gui.Parent = game:GetService("Players").LocalPlayer.PlayerGui
 -- Criar o botão
 local botao = Instance.new("TextButton")
 botao.Name = "BotaoPular"
-botao.Text = "Pular 3 vezes"
-botao.Size = UDim2.new(0, 150, 0, 50)
-botao.Position = UDim2.new(0.5, -75, 0.8, 0)
+botao.Text = ""
+botao.Size = UDim2.new(0, 50, 0, 50)
+botao.Position = UDim2.new(0.5, -25, 0.8, 0)
+botao.BackgroundTransparency = 1
+botao.ImageColor3 = Color3.new(1, 1, 1)
+
+-- Adicionar uma imagem ao botão
+local imagem = Instance.new("ImageLabel")
+imagem.Name = "Imagem"
+imagem.Size = UDim2.new(1, 0, 1, 0)
+imagem.BackgroundTransparency = 1
+imagem.Parent = botao
+
+-- Adicionar uma máscara de recorte ao botão para torná-lo redondo
+local mascara = Instance.new("ImageLabel")
+mascara.Name = "Mascara"
+mascara.Size = UDim2.new(1, 0, 1, 0)
+mascara.BackgroundTransparency = 1
+mascara.Image = "rbxassetid://6023426915" -- Substitua este ID pela ID da imagem da máscara de recorte
+mascara.Parent = botao
+botao.ClipsDescendants = true
+
 botao.Parent = gui
+
+-- Adicionar a função para abrir a galeria de imagens ao botão
+botao.MouseButton1Click:Connect(function()
+    local userInputService = game:GetService("UserInputService")
+    local selectedImage = userInputService:OpenImageSelector()
+
+    if selectedImage then
+        imagem.Image = selectedImage
+     end
+   end)
 
 -- Adicionar a função PularTresVezes ao botão
 botao.MouseButton1Click:Connect(function()
-    PularTresVezes()
+    PularTresVezes()       
 end)
-        
-end
